4954c900fe57cebcce1c84ee598fa5b7aa1bb398
ahmad <ahmadmohammadi940@gmail.com>
2019-06-25 00:25:30 +0430

    loop anf if statements (accepted)

--- loops.py
+++ loops.py
@@ -14,6 +14,7 @@ for word in words:
         print(char, end='')
     print()
 """
+"""
 n = input()
 n= int(n)
 if( n>= 1 & n<=100):
@@ -21,4 +22,19 @@ if( n>= 1 & n<=100):
         for j in range(1, n+1):
             print(i*j," ", end='')
         print()
+    pass
+"""
+n, d = map(int, input().split())
+if( (n >= 2 and n <= 100) or (d >= 1 and d<=1000)):
+    p =0
+    i=0
+    while True:
+        i+=1
+        p += d
+        r = p % n
+        if(r >= 0 and r <= n/2):
+            # print(i," ",p)
+            print(p)
+            break
+        pass
     pass
