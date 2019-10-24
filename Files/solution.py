e1e39b7f349a579efac5e6ddb10ec7c905a8f6fa
ahmad <ahmadmohammadi940@gmail.com>
2019-10-25 00:02:36 +0330

    تعداد دستورات کد پایتون

--- Files/solution.py
+++ Files/solution.py
@@ -4,21 +4,20 @@ def solve(address):
     some = 0
 
     while True:
-        line = f.readline()
-        if line != '\n' and line != '':
-            if not re.findall("([\s]+\n)", line):
-                if line.strip() == '#' or not re.findall("(#[.]*)", line):
+        linee = f.readline()
+        t = len(linee.strip())
+        if linee != '\n' and linee != '':
+            if not re.findall("([\s]+\n)", linee) or t > 0:
+                if linee.strip() == '#' or not re.findall("(#[.]*)", linee):
                     some += 1
-                    # print(re.findall("(#[a-zA-Z0-9])", line.strip()))
-                elif line.strip().find('#') > 0:
+                elif linee.strip().find('#') > 0:
                     some += 1
-                    # print(line.strip())
-
-                # print(re.findall("(\s+)", line))
-        if line == '':
+        # print(t)
+        if linee == '':
             break
         pass
     return some
     pass
 # print(solve("../functions.py"))
 # print(solve("f.txt"))
+# print(solve("TEST1.py"))
