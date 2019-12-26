e1fa0bb809cb221db463222b76a159c111f235e8
ahmadam <ahmadmohammadi940@gmail.com>
2019-12-26 10:50:18 +0330

    تمرین ۸ - فرزاد فیلم بین

---
+++ review_excercises/farzad_film.py
@@ -0,0 +1,9 @@
+n = int(input())
+for i in range(n):
+    text = input()
+    print(text.title())
+    pass
+
+"""
+for i in range(int(input())): print(" ".join([s.capitalize() for s in input().split()]))
+"""
